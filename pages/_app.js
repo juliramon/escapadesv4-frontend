2ecdcb2b4b4f66395441dda64f17e11c6806c4dd
Juli Ramon <juli@nexumlabs.com>
2020-12-27 22:54:54 +0100

    implemented histories form + started historia layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,77 +1,79 @@
 import "../styles/globals.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Router, {useRouter} from "next/router";
-import {useEffect, useState} from "react";
-import {useCookies} from "react-cookie";
+import "react-quill/dist/quill.bubble.css";
+
+import Router, { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
 import UserContext from "../contexts/UserContext";
 import AuthService from "../services/authService";
 
-function MyApp({Component, pageProps}) {
-	const [cookies, setCookie, removeCookie] = useCookies("");
-	let loggedData;
-	if (cookies.loggedInUser && cookies.loggedInUser !== null) {
-		loggedData = cookies.loggedInUser;
-	}
-	const initialState = {
-		loggedUser: loggedData,
-	};
+function MyApp({ Component, pageProps }) {
+  const [cookies, setCookie, removeCookie] = useCookies("");
+  let loggedData;
+  if (cookies.loggedInUser && cookies.loggedInUser !== null) {
+    loggedData = cookies.loggedInUser;
+  }
+  const initialState = {
+    loggedUser: loggedData,
+  };
 
-	const [state, setState] = useState(initialState);
+  const [state, setState] = useState(initialState);
 
-	let cookieCreationDate = new Date();
-	let cookieExpirationDate = new Date();
-	cookieExpirationDate.setFullYear(cookieCreationDate.getFullYear() + 1);
+  let cookieCreationDate = new Date();
+  let cookieExpirationDate = new Date();
+  cookieExpirationDate.setFullYear(cookieCreationDate.getFullYear() + 1);
 
-	const getLoggedUser = (user) => {
-		setState({...state, userFetched: true});
-		setCookie("loggedInUser", user, {expires: cookieExpirationDate});
-		Router.push("/feed");
-	};
+  const getLoggedUser = (user) => {
+    setState({ ...state, userFetched: true });
+    setCookie("loggedInUser", user, { expires: cookieExpirationDate });
+    Router.push("/feed");
+  };
 
-	const getNewUser = (user) => {
-		setState({...state, userFetched: true});
-		setCookie("loggedInUser", user, {expires: cookieExpirationDate});
-		Router.push("/signup/complete-account");
-	};
+  const getNewUser = (user) => {
+    setState({ ...state, userFetched: true });
+    setCookie("loggedInUser", user, { expires: cookieExpirationDate });
+    Router.push("/signup/complete-account");
+  };
 
-	useEffect(() => {
-		if (state.userFetched) {
-			setState({...state, loggedUser: cookies.loggedInUser});
-		}
-	}, [cookies]);
+  useEffect(() => {
+    if (state.userFetched) {
+      setState({ ...state, loggedUser: cookies.loggedInUser });
+    }
+  }, [cookies]);
 
-	const logOut = () => {
-		const service = new AuthService();
-		service
-			.logout()
-			.then(() => {
-				setCookie("loggedInUser", undefined, {expires: cookieExpirationDate});
-				removeCookie("loggedInUser");
-				Router.push("/login");
-			})
-			.catch((err) => console.log(err));
-	};
+  const logOut = () => {
+    const service = new AuthService();
+    service
+      .logout()
+      .then(() => {
+        setCookie("loggedInUser", undefined, { expires: cookieExpirationDate });
+        removeCookie("loggedInUser");
+        Router.push("/login");
+      })
+      .catch((err) => console.log(err));
+  };
 
-	const refreshUserData = (updatedUser) => {
-		console.log('updating user...')
-		removeCookie("loggedInUser");
-		setCookie("loggedInUser", updatedUser, {expires: cookieExpirationDate});
-		setState({...state, loggedUser: updatedUser});
-	};
+  const refreshUserData = (updatedUser) => {
+    console.log("updating user...");
+    removeCookie("loggedInUser");
+    setCookie("loggedInUser", updatedUser, { expires: cookieExpirationDate });
+    setState({ ...state, loggedUser: updatedUser });
+  };
 
-	return (
-		<UserContext.Provider
-			value={{
-				user: state.loggedUser,
-				saveUserDetails: getLoggedUser,
-				getNewUser: getNewUser,
-				refreshUserData: refreshUserData,
-				logOut: logOut,
-			}}
-		>
-			<Component {...pageProps} />;
-		</UserContext.Provider>
-	);
+  return (
+    <UserContext.Provider
+      value={{
+        user: state.loggedUser,
+        saveUserDetails: getLoggedUser,
+        getNewUser: getNewUser,
+        refreshUserData: refreshUserData,
+        logOut: logOut,
+      }}
+    >
+      <Component {...pageProps} />;
+    </UserContext.Provider>
+  );
 }
 
 export default MyApp;
