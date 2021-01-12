f81174940e17ba26a4724c987f5c8a767e5fe1cb
Juli Ramon <juli@nexumlabs.com>
2021-01-12 20:15:42 +0100

    updated

--- components/modals/PublicationModal.js
+++ components/modals/PublicationModal.js
@@ -5,21 +5,20 @@ import { useContext } from "react";
 
 const PublicationModal = ({ visibility, hideModal }) => {
   const { user } = useContext(UserContext);
-  console.log(user);
   const postStoryOption = (
     <li>
       <div className="col left">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          class="icon icon-tabler icon-tabler-notebook"
+          className="icon icon-tabler icon-tabler-notebook"
           width="44"
           height="44"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="#2c3e50"
           fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         >
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
           <path d="M6 4h11a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-11a1 1 0 0 1 -1 -1v-14a1 1 0 0 1 1 -1m3 0v18" />
@@ -122,7 +121,7 @@ const PublicationModal = ({ visibility, hideModal }) => {
               </Link>
             </div>
           </li>
-          {user.userType.includes("admin") ? postStoryOption : null}
+          {user.userType === "admin" ? postStoryOption : null}
         </ul>
       </Modal.Body>
     </Modal>
