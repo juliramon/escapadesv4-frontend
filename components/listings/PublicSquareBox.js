fee65ff84d464874d28443bb4d1a42993c47ccd9
Juli Ramon <juli@nexumlabs.com>
2021-03-11 19:54:31 +0100

    updated publicsquarebox styles + added call to actions

--- components/listings/PublicSquareBox.js
+++ components/listings/PublicSquareBox.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import styles from "../../styles/PublicSquareBox.module.scss";
 
 const PublicSquareBox = ({
   type,
@@ -11,21 +12,67 @@ const PublicSquareBox = ({
   location,
 }) => {
   let shortenedSubtitle = subtitle.slice(0, 70);
-  let linkPath;
+  let shortenedLocation;
+  if (location.props.children.length > 35) {
+    shortenedLocation = location.props.children.slice(0, 35) + "...";
+  } else {
+    shortenedLocation = location.props.children;
+  }
+  let linkPath, tipus, icon;
   if (type === "activity") {
     linkPath = "activitats";
+    tipus = "Activitat";
+    icon = (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="icon icon-tabler icon-tabler-route"
+        width="16"
+        height="16"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="#2c3e50"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <circle cx="6" cy="19" r="2" />
+        <circle cx="18" cy="5" r="2" />
+        <path d="M12 19h4.5a3.5 3.5 0 0 0 0 -7h-8a3.5 3.5 0 0 1 0 -7h3.5" />
+      </svg>
+    );
   } else if (type === "place") {
     linkPath = "allotjaments";
+    tipus = "Allotjament";
+    icon = (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="icon icon-tabler icon-tabler-bed"
+        width="18"
+        height="18"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="#2c3e50"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <path d="M3 7v11m0 -4h18m0 4v-8a2 2 0 0 0 -2 -2h-8v6" />
+        <circle cx="7" cy="10" r="1" />
+      </svg>
+    );
   }
   return (
-    <div id="listingSquareBox">
+    <div id="listingSquareBox" className={styles.listingSquareBox}>
       <Link href={`/${linkPath}/${slug}`}>
-        <a className="listing-box-wrapper">
+        <a className={styles.listingBoxWrapper}>
           <div
-            className="listing-cover"
+            className={styles.listingCover}
             style={{ backgroundImage: `url('${cover}')` }}
           ></div>
-          <div className="listing-rating-wrapper">
+
+          {/* <div className={styles.listingRatingWrapper}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="icon icon-tabler icon-tabler-star"
@@ -45,11 +92,37 @@ const PublicSquareBox = ({
                 d="M12 17.75l-6.172 3.245 1.179-6.873-4.993-4.867 6.9-1.002L12 2l3.086 6.253 6.9 1.002-4.993 4.867 1.179 6.873z"
               />
             </svg>
-            <span className="listing-rating">{rating}</span>
+            <span className={styles.listingRating}>{rating}</span>
+          </div> */}
+          <h3 className={styles.listingTitle}>{title}</h3>
+          <p className={styles.listingSubtitle}>{shortenedSubtitle}...</p>
+          <p className={styles.listingLocation}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-map-pin"
+              width="15"
+              height="15"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="#2c3e50"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <circle cx="12" cy="11" r="3" />
+              <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z" />
+            </svg>
+            {shortenedLocation}
+          </p>
+          <div className={styles.buttons}>
+            <a href="#" title="Detalls" className={styles.buttonDark}>
+              Detalls
+            </a>
+            <a href="#" title="Reservar" className={styles.buttonLight}>
+              Reservar
+            </a>
           </div>
-          <h3 className="listing-title">{title}</h3>
-          <p className="listing-subtitle">{shortenedSubtitle}...</p>
-          {location}
         </a>
       </Link>
     </div>
