7f077ae3083e1431afa7ca6ff4f85e8b6b1ef8f9
Juli Ramon <juli@nexumlabs.com>
2021-03-18 22:36:24 +0100

    updated

--- services/contentService.js
+++ services/contentService.js
@@ -3,7 +3,7 @@ const { default: Axios } = require("axios");
 class ContentService {
   constructor() {
     let service = Axios.create({
-      baseURL: process.env.API_URL,
+      baseURL: "https://escapadesenparella.herokuapp.com/api",
       withCredentials: true,
     });
     this.service = service;
