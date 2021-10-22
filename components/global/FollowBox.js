edecaf0899908458e274b185595e7acd8388534a
Juli Ramon <juli@nexumlabs.com>
2021-10-22 23:33:37 +0200

    fix: broken close button on follow box

--- components/global/FollowBox.js
+++ components/global/FollowBox.js
@@ -26,8 +26,8 @@ const FollowBox = () => {
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="icon icon-tabler icon-tabler-x"
-            width="20"
-            height="20"
+            width="26"
+            height="26"
             viewBox="0 0 24 24"
             strokeWidth="1.5"
             stroke="#00206B"
@@ -39,7 +39,6 @@ const FollowBox = () => {
             <line x1="18" y1="6" x2="6" y2="18" />
             <line x1="6" y1="6" x2="18" y2="18" />
           </svg>
-          Tancar
         </button>
         <div className="col-left">
           <div className="image-area">
