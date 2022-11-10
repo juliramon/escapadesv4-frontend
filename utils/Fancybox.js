fa9dc45a7ccad2a447be4344e7e31c8199f7ec71
Juli Ramon <juli@nexumlabs.com>
2022-11-10 22:59:12 +0100

    fix: front --> module load fancybox util

--- utils/Fancybox.js
+++ utils/Fancybox.js
@@ -3,20 +3,20 @@ import { useEffect } from "react";
 import { Fancybox as NativeFancybox } from "@fancyapps/ui/dist/fancybox.esm.js";
 import "@fancyapps/ui/dist/fancybox.css";
 
-function Fancybox(props) {
-  const delegate = props.delegate || "[data-fancybox]";
+function FancyboxUtil(props) {
+	const delegate = props.delegate || "[data-fancybox]";
 
-  useEffect(() => {
-    const opts = props.options || {};
+	useEffect(() => {
+		const opts = props.options || {};
 
-    NativeFancybox.bind(delegate, opts);
+		NativeFancybox.bind(delegate, opts);
 
-    return () => {
-      NativeFancybox.destroy();
-    };
-  }, []);
+		return () => {
+			NativeFancybox.destroy();
+		};
+	}, []);
 
-  return <>{props.children}</>;
+	return <>{props.children}</>;
 }
 
-export default Fancybox;
+export default FancyboxUtil;
