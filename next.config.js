55a25da9358b69a541cd5ba88f7c31fd9ee25028
Juli Ramon <juli@nexumlabs.com>
2023-03-02 21:24:59 +0100

    fix: front --> next.config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,12 @@
 const withTM = require("next-transpile-modules")(["@fancyapps/ui"]);
 
-const nextConfig = {
+module.exports = withTM({
 	env: {
 		API_URL: process.env.NEXT_PUBLIC_APP_API_URL,
 		GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
 		GOOGLE_ANALYTICS_TAG: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
 		STRIPE_API_KEY: process.env.NEXT_PUBLIC_STRIPE_API_KEY,
 	},
-};
-
-(module.exports = {
 	async redirects() {
 		return [
 			{
@@ -19,5 +16,4 @@ const nextConfig = {
 			},
 		];
 	},
-}),
-	withTM(nextConfig);
+});
