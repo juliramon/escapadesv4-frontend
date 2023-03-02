cb3d4eab55736d490cadb4107c92af75139de492
Juli Ramon <juli@nexumlabs.com>
2023-03-02 21:21:03 +0100

    add: front --> 301 redirect

--- next.config.js
+++ next.config.js
@@ -1,12 +1,23 @@
 const withTM = require("next-transpile-modules")(["@fancyapps/ui"]);
 
 const nextConfig = {
-  env: {
-    API_URL: process.env.NEXT_PUBLIC_APP_API_URL,
-    GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
-    GOOGLE_ANALYTICS_TAG: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
-    STRIPE_API_KEY: process.env.NEXT_PUBLIC_STRIPE_API_KEY,
-  },
+	env: {
+		API_URL: process.env.NEXT_PUBLIC_APP_API_URL,
+		GOOGLE_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+		GOOGLE_ANALYTICS_TAG: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
+		STRIPE_API_KEY: process.env.NEXT_PUBLIC_STRIPE_API_KEY,
+	},
 };
 
-module.exports = withTM(nextConfig);
+(module.exports = {
+	async redirects() {
+		return [
+			{
+				source: "/festivals-de-musica-a-catalunya",
+				destination: "/llistes/festivals-musica-catalunya",
+				permanent: true,
+			},
+		];
+	},
+}),
+	withTM(nextConfig);
