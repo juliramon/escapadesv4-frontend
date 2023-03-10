98f798cde324733321b72e1c4d6930c4d925abac
Juli <juli@nexumlabs.com>
2023-03-10 22:27:13 +0100

    add: front --> import Router new activity form

--- pages/nova-activitat.js
+++ pages/nova-activitat.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { useRouter } from "next/router";
+import Router, { useRouter } from "next/router";
 import { useState, useEffect, useContext } from "react";
 import NavigationBar from "../components/global/NavigationBar";
 import UserContext from "../contexts/UserContext";
