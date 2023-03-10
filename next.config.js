191fac515f9c9788642a831f0bdf40424d07d6d7
Juli <juli@nexumlabs.com>
2023-03-10 22:59:52 +0100

    add: front --> new 301 redirects

--- next.config.js
+++ next.config.js
@@ -14,6 +14,46 @@ module.exports = withTM({
 				destination: "/llistes/festivals-musica-catalunya",
 				permanent: true,
 			},
+			{
+				source: "/llistes/[slug]",
+				destination: "/llistes",
+				permanent: true,
+			},
+			{
+				source: "/escapades-culturals/2017/pastorets-de-mataro",
+				destination: "/escapades-culturals",
+				permanent: true,
+			},
+			{
+				source: "/categories-escapades/escapades-culturals/",
+				destination: "/escapades-culturals",
+				permanent: true,
+			},
+			{
+				source: "/listing-region/barcelona/?search_keywords=&search_region=178&search_categories[]=&filter_job_type[]=",
+				destination: "/escapades-catalunya/escapades-barcelona",
+				permanent: true,
+			},
+			{
+				source: "/escapades-tarragona",
+				destination: "/escapades-catalunya/escapades-tarragona",
+				permanent: true,
+			},
+			{
+				source: "/listing-type/refugi?search_keywords=&search_region=178&search_categories[]=179&filter_job_type[]=refugi",
+				destination: "/refugis",
+				permanent: true,
+			},
+			{
+				source: "/listing-type/refugi/?search_keywords=&search_region=182&search_categories[]=&filter_job_type[]=refugi",
+				destination: "/refugis",
+				permanent: true,
+			},
+			{
+				source: "/tag/aventura/",
+				destination: "/escapades-aventura",
+				permanent: true,
+			},
 		];
 	},
 });
