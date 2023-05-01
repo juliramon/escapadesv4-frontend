ccfb7d8de85c19c2a14ac7cbbb9eb16ae151f13a
Juli <juli@nexumlabs.com>
2023-05-01 11:46:15 +0200

    fix: front --> pagespeed optimization history

--- components/global/FooterHistoria.js
+++ components/global/FooterHistoria.js
@@ -1,61 +1,73 @@
 import React from "react";
 
 const FooterHistoria = () => {
-	return (
-		<div className="cta-instagram-footer pt-8 pb-14 md:pt-12 md:pb-20 flex flex-col items-center mt-8 md:mt-10 border-t border-primary-100">
-			<h2 className="text-center max-w-lg mx-auto">
-				Descobreix més escapades com aquesta. Segueix-nos a Instagram!
-			</h2>
-			<div className="mt-4 mb-1.5">
-				<a
-					href="https://instagram.com/escapadesenparella"
-					title="Segueix Escapadesenparella.cat a Instagram"
-					className="flex items-center"
-					rel="nofollow"
-					target="_blank"
-				>
-					@escapadesenparella.cat{" "}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						className="icon icon-tabler icon-tabler-arrow-narrow-right ml-1"
-						width="30"
-						height="30"
-						viewBox="0 0 24 24"
-						strokeWidth="1.5"
-						stroke="#F03E51"
-						fill="none"
-						strokeLinecap="round"
-						strokeLinejoin="round"
-					>
-						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-						<line x1="5" y1="12" x2="19" y2="12" />
-						<line x1="15" y1="16" x2="19" y2="12" />
-						<line x1="15" y1="8" x2="19" y2="12" />
-					</svg>
-				</a>
-			</div>
-			<div className="flex items-center mt-10 banner-wrapper">
-				<div className="relative rounded-md overflow-hidden left">
-					<img
-						source="/escapadesenparella-tossa-mar_m2lvdz-s.webp"
-						type="image/webp"
-						src="/escapadesenparella-tossa-mar_m2lvdz-s.jpg"
-						alt="Segueix-nos a Instagram"
-						loading="lazy"
-					/>
-				</div>
-				<div className="relative rounded-md overflow-hidden right">
-					<img
-						source="/escapadesenparella-comes-rubio_luuish-s.webp"
-						type="image/webp"
-						src="/escapadesenparella-comes-rubio_luuish-s.jpg"
-						alt="Segueix @escapadesenparella a Instagram"
-						loading="lazy"
-					/>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="cta-instagram-footer pt-8 pb-14 md:pt-12 md:pb-20 flex flex-col items-center mt-8 md:mt-10 border-t border-primary-100">
+      <h2 className="text-center max-w-lg mx-auto">
+        Descobreix més escapades com aquesta. Segueix-nos a Instagram!
+      </h2>
+      <div className="mt-4 mb-1.5">
+        <a
+          href="https://instagram.com/escapadesenparella"
+          title="Segueix Escapadesenparella.cat a Instagram"
+          className="flex items-center"
+          rel="nofollow"
+          target="_blank"
+        >
+          @escapadesenparella.cat{" "}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-arrow-narrow-right ml-1"
+            width="30"
+            height="30"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#F03E51"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <line x1="5" y1="12" x2="19" y2="12" />
+            <line x1="15" y1="16" x2="19" y2="12" />
+            <line x1="15" y1="8" x2="19" y2="12" />
+          </svg>
+        </a>
+      </div>
+      <div className="flex items-center mt-10 banner-wrapper">
+        <div className="relative rounded-md overflow-hidden left">
+          <picture>
+            <source
+              srcSet="/escapadesenparella-tossa-mar_m2lvdz-s.webp"
+              type="image/webp"
+            />
+            <img
+              src="/escapadesenparella-tossa-mar_m2lvdz-s.jpg"
+              alt="Segueix-nos a Instagram"
+              width={400}
+              height={300}
+              loading="lazy"
+            />
+          </picture>
+        </div>
+        <div className="relative rounded-md overflow-hidden right">
+          <picture>
+            <source
+              srcSet="/escapadesenparella-comes-rubio_luuish-s.webp"
+              type="image/webp"
+            />
+            <img
+              src="/escapadesenparella-comes-rubio_luuish-s.jpg"
+              alt="Segueix @escapadesenparella a Instagram"
+              width={400}
+              height={300}
+              loading="lazy"
+            />
+          </picture>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FooterHistoria;
