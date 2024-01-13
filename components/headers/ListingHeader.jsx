5b3e35acaf56e65072fc82de2d8d7a3e8cb88c8e
Juli <juli@nexumlabs.com>
2024-01-13 17:29:41 +0100

    add: front --> hide adbanner listing category on mobile devices

--- components/headers/ListingHeader.jsx
+++ components/headers/ListingHeader.jsx
@@ -15,9 +15,11 @@ const ListingHeader = ({ title, subtitle, sponsorData }) => {
 					<div
 						className="py-1.5 md:py-2.5 w-full md:w-1/2"
 					>
-						<AdBanner data-ad-slot="8648501664"
-							data-ad-format="auto"
-							data-full-width-responsive="true" />
+						<div className='hidden md:block'>
+							<AdBanner data-ad-slot="8648501664"
+								data-ad-format="auto"
+								data-full-width-responsive="true" />
+						</div>
 					</div>
 				</div>
 			</div>
